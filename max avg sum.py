68453c562477338d9d09fe79033e8c0ae5374f4c
Dhakshatha16 <dhaksha1624@gmail.com>
2026-03-09 15:26:04 +0530

    643

---
+++ max avg sum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        w_sum=sum(nums[:k])
+        m_sum=w_sum
+        for i in range(k,len(nums)):
+            w_sum=w_sum+nums[i]-nums[i-k]
+            m_sum=max(m_sum,w_sum)
+        return m_sum/float(k)
+
+
+        
