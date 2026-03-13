bab2c7f6d48147109f511ae02d90121ab53a6863
Dhakshatha16 <dhaksha1624@gmail.com>
2026-03-13 19:31:46 +0530

    6

---
+++ 599. Minimum Index Sum of Two Lists.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findRestaurant(self, list1, list2):
+        res = []
+        min_sum = float('inf')
+
+        for i in range(len(list1)):
+            for j in range(len(list2)):
+                if list1[i] == list2[j]:
+                    if i + j < min_sum:
+                        min_sum = i + j
+                        res = [list1[i]]
+                    elif i + j == min_sum:
+                        res.append(list1[i])
+        
+        return res
