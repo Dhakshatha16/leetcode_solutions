16c5f441bc1dd4ea914a1904623c2b3768c75602
Dhakshatha16 <dhaksha1624@gmail.com>
2026-04-22 09:34:03 +0530

    99

---
+++ database/1731. The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,7 @@
+select e.employee_id,e.name,count(m.employee_id)as reports_count,round(avg(m.age)) as average_age
+from employees e
+join employees m
+on m.reports_to=e.employee_id
+group by e.employee_id,e.name
+order by e.employee_id;
+
